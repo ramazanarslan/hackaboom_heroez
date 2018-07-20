8eba3a556c3d441088b575858242e2987cca6836
cerob <cerob@users.noreply.github.com>
2018-07-20 23:05:23 +0300

    add initial models

--- heroapi/heroapi/heroapiapp/models.py
+++ heroapi/heroapi/heroapiapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+CLOTH_TYPES= (
+    ('GE', 'General'),
+    ('SH', 'Short'),
+    ('TE', 'T-Shirt'),
+    ('PA', 'Pants'),
+    ('SO', 'Socks'),
+    ('UN', 'Underwear'),
+    ('HO', 'Shoes'),
+    ('SR', 'Shirts'),
+)
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    lat = models.IntegerField()
+    long = models.IntegerField()
+
+
+class Cloth(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.FileField()
+    xxl = models.BooleanField()
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    cloth_type = models.CharField(choices=CLOTH_TYPES, max_length=2, default='GE')
