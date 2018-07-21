a556b151de2d50ceb09586553e50b5d04ba3837d
Ramazan Arslan <ramazan@birlikte.al>
2018-07-21 13:06:11 +0300

    clothes page

--- hackaboom_app/src/screens/Clothes/index.js
+++ hackaboom_app/src/screens/Clothes/index.js
@@ -1,27 +1,32 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {
     View, FlatList, Card, CardItem,
-    TouchableOpacity
+    TouchableOpacity, ImageBackground
 } from "react-native";
 import {
     Container,
     Text, Icon, Right, Left, Body
 } from 'native-base';
 
-import { connect } from "react-redux";
-import { tryGetShops, getShopsReset } from "../../redux/clothes/Actions";
+import {connect} from "react-redux";
+import {tryGetShops, getShopsReset} from "../../redux/clothes/Actions";
 import {Navigation} from 'react-native-navigation';
 
-import MapView, { Marker } from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 
 import styles from "./styles";
 
-class Clothes extends Component {
+const bg = require("../../../assets/bggradient.png");
+
+
+class Clothes extends Component
+    {
     static navigatorStyle = {
         navBarHidden: true,
     };
 
-    constructor(props) {
+    constructor(props)
+        {
         super(props);
 
         this.state = {
@@ -32,71 +37,84 @@ class Clothes extends Component {
                 longitudeDelta: 0.0421,
             }
         }
-    }
+        }
 
-    componentDidMount() {
-        const { shopList } = this.props.clothes;
-        if (shopList.length <= 0)
+
+    componentDidMount()
+        {
+        const {shopList} = this.props.clothes;
+        if ( shopList.length <= 0 )
             this.props.tryGetShops();
-    }
+        }
 
-    componentDidUpdate() {
+    componentDidUpdate()
+        {
         const {
             getShopsInProgress, getShopsHasError, getShopsCompleted
         } = this.props.clothes;
 
-        if (!getShopsInProgress && !getShopsHasError && getShopsCompleted) {
+        if ( !getShopsInProgress && !getShopsHasError && getShopsCompleted )
+            {
             console.log("fetching shop is done...");
             this.props.getShopsReset();
-        }
-        else if (!getShopsInProgress && getShopsHasError && getShopsCompleted) {
+            }
+        else if ( !getShopsInProgress && getShopsHasError && getShopsCompleted )
+            {
             console.log("fetching shop has error...");
             this.props.getShopsReset();
+            }
         }
-    }
 
-    render() {
-        const { shopList } = this.props.clothes;
+    render()
+        {
+        const {shopList} = this.props.clothes;
 
         return (
             <Container>
+
+
                 <View style={styles.mapLayout}>
                     <MapView
                         style={styles.mapStyle}
                         region={this.state.region}
-                        onRegionChangeComplete={(region) => this.setState({ region })}>
+                        onRegionChangeComplete={(region) => this.setState({region})}>
                         {shopList.map(item => (
                             <Marker
                                 key={item.id}
-                                coordinate={{ latitude: parseFloat(item.lat), longitude: parseFloat(item.long) }}
+                                coordinate={{latitude: parseFloat(item.lat), longitude: parseFloat(item.long)}}
                                 title={item.name}
                             />
                         ))}
                     </MapView>
                 </View>
+
                 <View style={styles.listLayout}>
-                    <FlatList
-                        data={shopList}
-                        keyExtractor={(item, index) => item.id + "-" + index}
-                        renderItem={({ item, index }) => this.renderShopListItem(item, index)}
-                        ListEmptyComponent={() => this.renderEmptyListItem()}
-                    />
+                    <ImageBackground source={bg} resizeMode= 'stretch' style={styles.bg}>
+                        <FlatList
+                            data={shopList}
+                            keyExtractor={(item, index) => item.id + "-" + index}
+                            renderItem={({item, index}) => this.renderShopListItem(item, index)}
+                            ListEmptyComponent={() => this.renderEmptyListItem()}
+                        />
+                    </ImageBackground>
                 </View>
+
             </Container>
         );
-    }
+        }
 
-    renderShopListItem(item, index) {
+    renderShopListItem(item, index)
+        {
         return (
 
-            <TouchableOpacity onPress={() => this.goto_shopDetail(item.id)}>
+            <TouchableOpacity onPress={() => this.goto_shopDetail(index, item.name)}>
                 <View style={styles.card}>
 
                     <View style={styles.roundedNameInitials}><Text style={{
-                        color: "#fff",
+                        color: "#23c086",
                         fontSize: 19,
                         alignSelf: "center",
-                         fontFamily: 'Poppins-Regular'
+                        fontFamily: 'Poppins-Regular'
                     }}>{item.name.charAt(0).toUpperCase()}{item.name.charAt(0).toUpperCase()}</Text></View>
 
                     <Text style={styles.textname}>{item.name}</Text>
@@ -106,33 +124,37 @@ class Clothes extends Component {
 
             </TouchableOpacity>
         );
-    }
+        }
 
-    renderEmptyListItem() {
+    renderEmptyListItem()
+        {
         return (
             <View>
                 <Text>No item :(</Text>
             </View>
         );
-    }
+        }
 
-    goto_shopDetail(id) {
+    goto_shopDetail(id, name)
+        {
         this.props.navigator.push({
             screen: 'hackaboomapp.ShopDetail', // unique ID registered with Navigation.registerScreen
             animated: true, // does the push have transition animation or does it happen immediately (optional)
-            animationType: 'slide-down', // 'fade' (for both) / 'slide-horizontal' (for android) does the push have different transition animation (optional)
-            navigatorStyle: { navBarHidden: true }, // override the navigator style for the pushed screen (optional),
-            passProps: {shopId: id}
-          });
+            animationType: 'slide-down', // 'fade' (for both) / 'slide-horizontal' (for android) does the push have
+                                         // different transition animation (optional)
+            navigatorStyle: {navBarHidden: true}, // override the navigator style for the pushed screen (optional),
+            passProps: {shopId: id, shopName: name}
+        });
+        }
     }
-}
 
-function bindAction(dispatch) {
+function bindAction(dispatch)
+    {
     return {
         tryGetShops: () => dispatch(tryGetShops()),
         getShopsReset: () => dispatch(getShopsReset())
     }
-}
+    }
 
 const mapStateToProps = state => ({
     clothes: state.clothes
--- hackaboom_app/src/screens/Clothes/styles.js
+++ hackaboom_app/src/screens/Clothes/styles.js
@@ -1,5 +1,6 @@
-import {StyleSheet} from "react-native";
+import {Dimensions, StyleSheet} from "react-native";
 
+const deviceHeight = Dimensions.get("window").height;
 export default StyleSheet.create({
     mapLayout: {
         flex: 0.4
@@ -11,18 +12,21 @@ export default StyleSheet.create({
         flex: 1
     },
     card: {
-        marginLeft:5,
-        marginRight:5,
-        marginBottom:5,
-        marginTop:5,
-        padding: 15,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: 'rgba(0,0,0,0.2)',
-        height: 60,
-        backgroundColor: '#fff',
-        flexDirection: 'row',
-        elevation: 3,
+
+            justifyContent: 'space-between',
+            marginLeft: 5,
+            marginRight: 5,
+            marginBottom: 5,
+            marginTop: 5,
+            padding: 15,
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: 'rgba(255,255,255,0.1)',
+            backgroundColor: 'rgba(255,255,255,0.3)',
+            height: 60,
+            flexDirection: 'row',
+            elevation: 1
+
 
     },
     roundedNameInitials: {
@@ -33,16 +37,24 @@ export default StyleSheet.create({
         width: 40,
         height: 40,
         borderRadius: 40,
-        backgroundColor: "#23c086",
+        backgroundColor: "#fff",
         elevation: 3
     },
     textname: {
         fontFamily: 'Poppins-Regular',
-
+color:'white',
         marginLeft:5,
         alignSelf: 'center'
     },
     bodyname: {
     alignItems:'center'
-    }
+    },
+    bg: {
+        flex: 1,
+        width: null,
+        height: deviceHeight,
+        backgroundColor: "rgba(0,0,0,0.1)",
+        flexDirection: "row",
+        alignSelf: "center"
+    },
 });
